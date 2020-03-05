8b65e98284fa4a1aad0fa370bd7126cce1a1cdc9
shunchan0677 <shunchan0677@gmail.com>
2020-03-06 00:18:01 +0900

    Default docker scripts

---
+++ docker/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Build Docker Image
+docker build -t infogail -f Dockerfile .
---
+++ docker/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/home/$USER/.Xauthority
+SHARED_DIR=/home/autoware/shared_dir
+HOST_DIR=/home/$USER/shared_dir
+
+
+nvidia-docker run \
+    -it --rm \
+    --volume=$XSOCK:$XSOCK:rw \
+    --volume=$XAUTH:$XAUTH:rw \
+    --volume=$HOST_DIR:$SHARED_DIR:rw \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY=${DISPLAY}" \
+    --net=host \
+    --device=/dev/video0:/dev/video0:rmw \
+    --privileged -v /dev/bus/usb:/dev/bus/usb \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --runtime=nvidia \
+    infogail
